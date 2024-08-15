27a20ce44f11b7f6a2f25ad59da76c659bf2202a
Edward Stables <edward.stables1198@gmail.com>
2024-08-16 00:31:56 +0100

    enable autocompletion

--- lua/myconfig/init.lua
+++ lua/myconfig/init.lua
@@ -4,3 +4,7 @@ require("myconfig.lsp")
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.cmd [[colorscheme evening]]
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
